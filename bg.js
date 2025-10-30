d15d41f544679bd30f0b63e37da8a7f0bf2bd43c
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-31 01:55:15 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,6 +1,6 @@
 {
   let run = (a, b) => {
-    let { url } = b || a;
+    let url = (b || a).url;
     chrome.windows.create({
       url: b || url[0] != "c" ? url : "chrome://newtab",
       incognito: !0
